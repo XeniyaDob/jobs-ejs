85643c55ed9fc11c6e857be7166a070e0f1f6944
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-01-01 10:00:46 -0500

    Set up Sessions

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config(); // to load the .env file into the process.env object
+const session = require("express-session");
 const express = require("express");
 require("express-async-errors");
 
@@ -7,12 +9,23 @@ app.set("view engine", "ejs");
 app.use(require("body-parser").urlencoded({ extended: true }));
 
 // secret word handling
-let secretWord = "syzygy";
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 app.get("/secretWord", (req, res) => {
-  res.render("secretWord", { secretWord });
+  //console.log("req.session.secretWord", req.session.secretWord);
+  if (!req.session.secretWord) {
+    req.session.secretWord = "syzygy";
+  }
+  res.render("secretWord", { secretWord: req.session.secretWord });
 });
 app.post("/secretWord", (req, res) => {
-  secretWord = req.body.secretWord;
+  req.session.secretWord = req.body.secretWord;
   res.redirect("/secretWord");
 });
 
