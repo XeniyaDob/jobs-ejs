062ce9fcdc6ffe2c9c8032cdbbf4e98b13420b04
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-04-23 05:36:36 -0400

    Add flash messages to inform a user

--- app.js
+++ app.js
@@ -35,15 +35,25 @@ if (app.get("env") === "production") {
 
 app.use(session(sessionParms));
 
+app.use(require("connect-flash")());
+
 app.get("/secretWord", (req, res) => {
-  //console.log("req.session.secretWord", req.session.secretWord);
   if (!req.session.secretWord) {
     req.session.secretWord = "syzygy";
   }
+  res.locals.info = req.flash("info");
+  res.locals.errors = req.flash("error");
   res.render("secretWord", { secretWord: req.session.secretWord });
 });
+
 app.post("/secretWord", (req, res) => {
-  req.session.secretWord = req.body.secretWord;
+  if (req.body.secretWord.toUpperCase()[0] == "P") {
+    req.flash("error", "That word won't work!");
+    req.flash("error", "You can't use words that start with p.");
+  } else {
+    req.session.secretWord = req.body.secretWord;
+    req.flash("info", "The secret word was changed.");
+  }
   res.redirect("/secretWord");
 });
 
