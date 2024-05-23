67e9b708959c779c7ad2dc26ac8614a54d9af4d0
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-05-23 09:52:31 -0400

    tests for registration and logon

--- tests/test_multiply_api.mjs
+++ tests/test_multiply_api.mjs
@@ -2,9 +2,13 @@ import { expect, use } from "chai";
 import chaiHttp from "chai-http";
 
 import { app, server } from "../app.js";
-
+import { factory, seed_db } from "../utils/seed_db.js";
 // Use chaiHttp plugin
 const chai = use(chaiHttp);
+
+import { faker } from "@faker-js/faker";
+
+import User from "../models/User.js";
 
 describe("test multiply api", function () {
   after(() => {
@@ -44,5 +48,73 @@ describe("test getting a page", function () {
         expect(res.text).to.include("Click this link");
         done();
       });
+  });
+});
+
+describe("tests for registration and logon", function () {
+  after(() => {
+    server.close();
+  });
+  it("should get the registration page", (done) => {
+    chai
+      .request(app)
+      .get("/sessions/register")
+      .send()
+      .end((err, res) => {
+        expect(err).to.equal(null);
+        expect(res).to.have.status(200);
+        expect(res).to.have.property("text");
+        expect(res.text).to.include("Enter your name:");
+        const textNoLineEnd = res.text.replaceAll("\n", "");
+        const csrfToken = /_csrf\" value=\"(.*?)\"/.exec(textNoLineEnd);
+        expect(csrfToken).to.not.be.null;
+        this.csrfToken = csrfToken[1];
+        expect(res).to.have.property("headers");
+        expect(res.headers).to.have.property("set-cookie");
+        const cookies = res.headers["set-cookie"];
+        const csrfCookie = cookies.find((element) =>
+          element.startsWith("csrfToken")
+        );
+        expect(csrfCookie).to.not.be.undefined;
+        const cookieValue = /csrfToken=(.*?);\s/.exec(csrfCookie);
+        this.csrfCookie = cookieValue[1];
+        done();
+      });
+  });
+
+  it("should register the user", async () => {
+    this.password = faker.internet.password();
+    this.user = await factory.build("user", { password: this.password });
+    const dataToPost = {
+      name: this.user.name,
+      email: this.user.email,
+      password: this.password,
+      password1: this.password,
+      _csrf: this.csrfToken,
+    };
+    try {
+      // const startTime = Date.now(); // Start timing
+      const request = chai
+        .request(app)
+        .post("/sessions/register")
+        .set("Cookie", `csrfToken=${this.csrfCookie}`)
+        .set("content-type", "application/x-www-form-urlencoded")
+        .send(dataToPost);
+
+      // console.log(request, ")))))");
+      const res = await request;
+      console.log("got here");
+      expect(res).to.have.status(200);
+      expect(res).to.have.property("text");
+      // console.log(res, ")))))");
+      expect(res.text).to.include("Items List");
+      //const newUserStartTime = Date.now(); // Start timing for database query
+      let newUser = await User.findOne({ email: this.user.email });
+      expect(newUser).to.not.be.null;
+      console.log(newUser);
+    } catch (err) {
+      console.log(err);
+      expect.fail("Register request failed");
+    }
   });
 });
